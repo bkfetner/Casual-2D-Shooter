a686bbdb60f71690be4668444253029c9a5ab60a
bkfetner <bkfetner@gmail.com>
2023-08-31 10:36:47 -0700

    Added enemy generation and basic movement

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const content = document.getElementById('content');
 const playerCharacter = document.createElement('div');
+const enemies = [];
+let enemyCount = 0;
+let nextEnemyPlacementLeft = 0;
+let nextEnemyPlacementTop = 0;
 let currentLeft = 100/2; // Initial left position in vw
 let currentTop = 56.25/2;  // Initial top position in vw
 const moveDistance = 0.2; // The distance to move in vw
@@ -16,6 +20,66 @@ function addPlayerCharacter() {
   // playerCharacter.style.backgroundColor = 'red';
   content.appendChild(playerCharacter);
   updatePlayerCharacterPosition();
+}
+
+function addEnemy() {
+  const enemy = document.createElement('div');
+  enemy.classList.add('enemy');
+  enemy.classList.add('e' + enemyCount);
+  enemyCount++;
+  enemy.style.width = '2vw';
+  enemy.style.height = '2vw';
+
+  let currentEnemyLeft = nextEnemyPlacementLeft;
+  let currentEnemyTop = nextEnemyPlacementTop;
+  enemy.style.left = currentEnemyLeft + 'vw';
+  enemy.style.top = currentEnemyTop + 'vw';
+  if(nextEnemyPlacementLeft == 0 && nextEnemyPlacementTop < 54) {
+    nextEnemyPlacementTop += 3;
+  } else if(nextEnemyPlacementTop == 54 && nextEnemyPlacementLeft < 96) {
+    nextEnemyPlacementLeft += 3;
+  } else if(nextEnemyPlacementLeft == 96 && nextEnemyPlacementTop > 0) {
+    nextEnemyPlacementTop -= 3;
+  } else {
+    nextEnemyPlacementLeft -=3;
+  }
+
+  enemies.push({div: enemy, currentEnemyTop: currentEnemyTop, currentEnemyLeft: currentEnemyLeft});
+  content.appendChild(enemy);
+}
+
+function moveEnemies() {
+  enemies.forEach((currEnemy) => {
+    let enemy = currEnemy.div;
+    let currentEnemyTop = currEnemy.currentEnemyTop;
+    let currentEnemyLeft = currEnemy.currentEnemyLeft;
+
+    if(currentEnemyTop < currentTop) {
+      currentEnemyTop += moveDistance/2;
+    } else if(currentEnemyTop > currentTop) {
+      currentEnemyTop -= moveDistance/2;
+    }
+
+    if(currentEnemyLeft < currentLeft) {
+      currentEnemyLeft += moveDistance/2;
+    } else if(currentEnemyLeft > currentLeft) {
+      currentEnemyLeft -= moveDistance/2;
+    }
+
+    enemy.style.left = currentEnemyLeft + 'vw';
+    enemy.style.top = currentEnemyTop + 'vw';
+    currEnemy.currentEnemyTop = currentEnemyTop;
+    currEnemy.currentEnemyLeft = currentEnemyLeft;
+  })
+}
+
+function updateEnemies() {
+  moveEnemies()
+
+  if(enemies.length < 100) {
+    addEnemy()
+  }
+  console.log(enemies);
 }
 
 function updatePlayerCharacterPosition() {
@@ -42,7 +106,12 @@ function movePlayerCharacter() {
   updatePlayerCharacterPosition();
 }
 
-let interval = setInterval(movePlayerCharacter, 16.667);
+function createNeeGameCycle() {
+  movePlayerCharacter()
+  updateEnemies()
+}
+
+let interval = setInterval(createNeeGameCycle, 16.667);
 
 document.addEventListener('keydown', keydown)
 document.addEventListener('keyup', keyup)
