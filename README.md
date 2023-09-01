613d4fd6b4dcbf494903df5e86df3be36183409f
bkfetner <bkfetner@gmail.com>
2023-09-01 06:17:43 -0700

    Added collision for bullets and enemies. Also added a health countdown and a Gameover screen.

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# Casual-2D-Shooter
+# Casual 2D Shooter Prototype
+
+Welcome to the Casual 2D Shooter Prototype repository! This simple game is built using JavaScript, HTML, and CSS, offering players a basic but engaging experience. The game features 8-directional player control, automatic firing of bullets, and a multitude of enemies in pursuit.
+
+The inspiration for this project comes from the game "Vampire Survivor," which popularized the casual 2D shooter genre. While this prototype captures the essence of the genre, there's still room for innovation and expansion.
+
+## Features
+
+- **8-Directional Control**: Players can move the character in eight different directions, providing fluid and responsive movement.
+
+- **Auto-Firing Bullets**: The player's character automatically fires bullets towards enemies, making gameplay more engaging and action-packed.
+
+- **Numerous Enemies**: The game features a large number of enemies that relentlessly pursue the player, creating a challenging environment.
+
+## Planned Improvements
+
+While this prototype demonstrates the core mechanics of the game, there are several areas for improvement that you can consider working on:
+
+- **Scrolling Backgrounds**: Implement scrolling backgrounds to add depth and visual interest to the game world.
+
+- **Explosion Animations**: Introduce explosion animations to enhance the visual impact of defeated enemies and create a more satisfying experience.
+
+- **Diverse Weapon Types**: Include different weapon types that players can acquire, each with its own unique capabilities.
+
+- **Variety of Enemy Types**: Add various enemy types, each with distinct behaviors and patterns, to increase gameplay variety.
+
+- **Code Organization and OOP**: Refactor the codebase to follow better organization practices and potentially incorporate object-oriented programming (OOP) principles for cleaner and more maintainable code.
+
+## Getting Started
+
+To try out the prototype, follow these steps:
+
+1. Clone the repository to your local machine using `git clone`.
+2. Open the `index.html` file in your web browser.
+3. Play around with the controls and explore the current gameplay features.
+
+## Contributing
+
+Contributions are more than welcome! If you have ideas for improvements, new features, or bug fixes, feel free to submit a pull request.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+## Acknowledgments
+
+- This project draws inspiration from the game "Vampire Survivor," which popularized the casual 2D shooter genre.
+
--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const content = document.getElementById('content');
-const playerCharacter = document.createElement('div');
-const bullets = [];
-const bulletFireRate = 500;
+let bullets = [];
+const bulletFireRate = 250;
+let bulletcount = 0;
 let lastTimeBulletFired = Date.now();
-const enemies = [];
+let lastBulletDirection = 'up';
+let enemies = [];
+let enemySpawnLocations = [];
 let enemyCount = 0;
-let nextEnemyPlacementLeft = 30; // set default to -3
-let nextEnemyPlacementTop = 30; // set default to -3
+const enemyLimit = 50;
+let nextEnemyPlacementLeft = -3; // set default to -3
+let nextEnemyPlacementTop = -3; // set default to -3
+let playerHealth = 100;
 let currentPlayerLeft = 100/2; // Initial left position in vw
 let currentPlayerTop = 56.25/2;  // Initial top position in vw
 const moveDistance = 0.2; // The distance to move in vw
@@ -18,12 +22,14 @@ let leftKey = false;
 let currentPlayerDirection = 'left';
 
 function addPlayerCharacter() {
+  const playerCharacter = document.createElement('div');
   playerCharacter.classList.add('playerCharacter');
   playerCharacter.style.width = '2vw';
   playerCharacter.style.height = '2vw';
   playerCharacter.style.backgroundColor = 'red';
   content.appendChild(playerCharacter);
-  updatePlayerCharacterPosition();
+  playerCharacter.style.top = currentPlayerTop + 'vw';
+  playerCharacter.style.left = currentPlayerLeft + 'vw';
 }
 
 function updatePlayerCharacterPosition() {
@@ -32,6 +38,9 @@ function updatePlayerCharacterPosition() {
 }
 
 function movePlayerCharacter() {
+  const playerCharacter = document.querySelector('.playerCharacter');
+  // console.log(playerCharacter);
+
   if(upKey && !downKey) {
     currentPlayerTop = currentPlayerTop - moveDistance;
     currentPlayerDirection = 'up';
@@ -49,22 +58,20 @@ function movePlayerCharacter() {
     currentPlayerDirection = 'left';
   }
 
-  updatePlayerCharacterPosition();
+  playerCharacter.style.top = currentPlayerTop + 'vw';
+  playerCharacter.style.left = currentPlayerLeft + 'vw';
 }
 
 function evaluatePlayerCharacterCollision() {
+  const playerCharacter = document.querySelector('.playerCharacter');
+  const enemies = document.querySelectorAll('.enemy');
+
   const currentPlayerBottom = currentPlayerTop + 2;
   const currentPlayerRight = currentPlayerLeft + 2;
 
-  if(currentPlayerLeft < 0) currentPlayerLeft = 0;
-  if(currentPlayerTop < 0) currentPlayerTop = 0;
-  if(currentPlayerBottom > 56.25) currentPlayerTop = 54.25;
-  if(currentPlayerRight > 100) currentPlayerLeft = 98;
-
   enemies.forEach((enemy) => {
-    let currEnemy = enemy.div;
-    let currentEnemyTop = enemy.currentEnemyTop;
-    let currentEnemyLeft = enemy.currentEnemyLeft;
+    let currentEnemyTop = parseFloat(enemy.style.top);
+    let currentEnemyLeft = parseFloat(enemy.style.left);
     let currentEnemyBottom = currentEnemyTop + 2;
     let currentEnemyRight = currentEnemyLeft + 2;
     
@@ -74,6 +81,10 @@ function evaluatePlayerCharacterCollision() {
       currentPlayerTop < currentEnemyBottom &&
       currentPlayerBottom > currentEnemyTop
     ) {
+      playerHealth -= 1;
+      let p = document.querySelector('.p');
+      p.innerHTML = 'Health: ' + playerHealth + '%';
+
       const leftOverlap = Math.abs(currentPlayerLeft - currentEnemyRight);
       const topOverlap = Math.abs(currentPlayerTop - currentEnemyBottom);
       const rightOverlap = Math.abs(currentPlayerRight - currentEnemyLeft);
@@ -118,63 +129,99 @@ function evaluatePlayerCharacterCollision() {
       }
     }
 
-    enemy.currentEnemyTop = currentEnemyTop;
-    enemy.currentEnemyLeft = currentEnemyLeft;
-    currEnemy.style.top = currentEnemyTop + 'vw';
-    currEnemy.style.left = currentEnemyLeft + 'vw';
+    enemy.style.top = currentEnemyTop + 'vw';
+    enemy.style.left = currentEnemyLeft + 'vw';
   })
 
-  updatePlayerCharacterPosition();
+  if(currentPlayerLeft < 0) currentPlayerLeft = 0;
+  if(currentPlayerTop < 0) currentPlayerTop = 0;
+  if(currentPlayerBottom > 56.25) currentPlayerTop = 54.25;
+  if(currentPlayerRight > 100) currentPlayerLeft = 98;
+
+  playerCharacter.style.top = currentPlayerTop + 'vw';
+  playerCharacter.style.left = currentPlayerLeft + 'vw';
 }
 
 function addBullet() {
+  const playerCharacter = document.querySelector('.playerCharacter');
+
   const bullet = document.createElement('div');
+  const id = 'bullet' + bulletcount;
   bullet.classList.add('bullet');
+  bullet.setAttribute('id', id);
+  bulletcount++;
   bullet.style.width = '1vw';
   bullet.style.height = '1vw';
   let currentBulletTop = parseFloat(playerCharacter.style.top) + 0.5;
   let currentBulletLeft = parseFloat(playerCharacter.style.left) + 0.5;
-  const bulletDirection = currentPlayerDirection;
-  const bulletSpeed = 0.4;
+  let bulletDirection = 'up';
+  if(lastBulletDirection === 'up') {
+    bulletDirection = 'right';
+    lastBulletDirection = 'right';
+  } else if(lastBulletDirection === 'right') {
+    bulletDirection = 'down';
+    lastBulletDirection = 'down';
+  } else if(lastBulletDirection === 'down') {
+    bulletDirection = 'left';
+    lastBulletDirection = 'left';
+  } else {
+    bulletDirection = 'up';
+    lastBulletDirection = 'up';
+  }
+  const bulletSpeed = moveDistance * 2;
 
+  bullet.setAttribute('bulletDirection', bulletDirection);
+  bullet.setAttribute('bulletSpeed', bulletSpeed);
   bullet.style.left = currentBulletLeft + 'vw';
   bullet.style.top = currentBulletTop + 'vw';
-  bullets.push({div: bullet, currentBulletTop: currentBulletTop, currentBulletLeft: currentBulletLeft, bulletDirection: bulletDirection, bulletSpeed: bulletSpeed});
+  // console.log(bullet);
+  // console.log(currentBulletTop + " " + currentBulletLeft + " " + bulletDirection + " " + bulletSpeed);
   content.appendChild(bullet);
 }
 
 function moveBullets() {
-  bullets.forEach((currBullet) => {
-    let bullet = currBullet.div;
-    let currentBulletTop = currBullet.currentBulletTop;
-    let currentBulletLeft = currBullet.currentBulletLeft;
-    let bulletDirection = currBullet.bulletDirection;
-    let bulletSpeed = currBullet.bulletSpeed;
+  const bullets = document.querySelectorAll('.bullet');
+
+  bullets.forEach((bullet) => {
+    // console.log(bullet);
+    let currentBulletTop = parseFloat(bullet.style.top);
+    let currentBulletLeft = parseFloat(bullet.style.left);
+    let bulletDirection = bullet.getAttribute('bulletDirection');
+    let bulletSpeed = parseFloat(bullet.getAttribute('bulletSpeed'));
+
+    // console.log(currentBulletTop + " " + currentBulletLeft + " " + bulletDirection + " " + bulletSpeed);
+    // console.log(bulletDirection);
 
     switch(bulletDirection) {
       case 'up':
+        // console.log('switch up');
         currentBulletTop -= bulletSpeed;
         break;
       case 'down':
+        // console.log('switch down');
         currentBulletTop += bulletSpeed;
         break;
       case 'left':
+        // console.log('switch left');
         currentBulletLeft -= bulletSpeed;
         break;
       case 'right':
+        // console.log('switch right');
         currentBulletLeft += bulletSpeed;
         break;
     }
 
-    bullet.style.left = currentBulletLeft + "vw";
-    bullet.style.top = currentBulletTop + "vw";
-    currBullet.currentBulletLeft = currentBulletLeft;
-    currBullet.currentBulletTop = currentBulletTop;
+    if(currentBulletTop < -10 || currentBulletTop > 57.5 || currentBulletLeft < -10 || currentBulletLeft > 110) {
+      bullet.remove();
+    } else {
+      bullet.style.left = currentBulletLeft + "vw";
+      bullet.style.top = currentBulletTop + "vw";
+    }
   })
 }
 
 function updateBullets() {
-  moveBullets()
+  moveBullets();
 
   const timeNow = Date.now();
   if(timeNow - lastTimeBulletFired >= bulletFireRate) {
@@ -186,34 +233,47 @@ function updateBullets() {
 function addEnemy() {
   const enemy = document.createElement('div');
   enemy.classList.add('enemy');
-  enemy.classList.add('e' + enemyCount);
+  const id = 'enemy' + enemyCount;
   enemyCount++;
+  enemy.setAttribute('id', id);
   enemy.style.width = '2vw';
   enemy.style.height = '2vw';
 
-  let currentEnemyLeft = nextEnemyPlacementLeft;
-  let currentEnemyTop = nextEnemyPlacementTop;
-  enemy.style.left = currentEnemyLeft + 'vw';
-  enemy.style.top = currentEnemyTop + 'vw';
-  if(nextEnemyPlacementLeft == -3 && nextEnemyPlacementTop < 57) {
-    nextEnemyPlacementTop += 3;
-  } else if(nextEnemyPlacementTop == 57 && nextEnemyPlacementLeft < 102) {
-    nextEnemyPlacementLeft += 3;
-  } else if(nextEnemyPlacementLeft == 102 && nextEnemyPlacementTop > -3) {
-    nextEnemyPlacementTop -= 3;
-  } else {
-    nextEnemyPlacementLeft -=3;
+  // set up spawn locations
+  if(enemySpawnLocations.length === 0) {
+    for(let i = -3; i <= 102; i++) {
+      enemySpawnLocations.push({col: i, row: -3});
+      enemySpawnLocations.push({col: i, row: 57});
+    }
+    for(let j = 0; j <= 54; j++) {
+      enemySpawnLocations.push({col: -3, row: j});
+      enemySpawnLocations.push({col: 102, row: j});
+    }
+    for(let i = 0; i < enemySpawnLocations.length; i++) {
+      let rand = Math.floor(Math.random() * enemySpawnLocations.length);
+      let temp = enemySpawnLocations[rand];
+      enemySpawnLocations[rand] = enemySpawnLocations[i];
+      enemySpawnLocations[i] = temp;
+    }
   }
 
-  enemies.push({div: enemy, currentEnemyTop: currentEnemyTop, currentEnemyLeft: currentEnemyLeft});
+  let spawnLocation = enemySpawnLocations[0];
+  enemySpawnLocations.splice(0, 1);
+  let currentEnemyLeft = spawnLocation.col;
+  let currentEnemyTop = spawnLocation.row;
+  enemy.style.left = currentEnemyLeft + 'vw';
+  enemy.style.top = currentEnemyTop + 'vw';
   content.appendChild(enemy);
 }
 
 function moveEnemies() {
-  enemies.forEach((currEnemy) => {
-    let enemy = currEnemy.div;
-    let currentEnemyTop = currEnemy.currentEnemyTop;
-    let currentEnemyLeft = currEnemy.currentEnemyLeft;
+  const enemies = document.querySelectorAll('.enemy');
+  // console.log(enemies);
+
+  enemies.forEach((enemy) => {
+    // console.log(enemy.id);
+    let currentEnemyTop = parseFloat(enemy.style.top);
+    let currentEnemyLeft = parseFloat(enemy.style.left);
 
     if(currentEnemyTop < currentPlayerTop) {
       currentEnemyTop += moveDistance/2;
@@ -227,36 +287,180 @@ function moveEnemies() {
       currentEnemyLeft -= moveDistance/2;
     }
 
+    // playerCharacter.style.top = currentPlayerTop + 'vw';
+    // playerCharacter.style.left = currentPlayerLeft + 'vw';
+
     enemy.style.left = currentEnemyLeft + 'vw';
     enemy.style.top = currentEnemyTop + 'vw';
-    currEnemy.currentEnemyTop = currentEnemyTop;
-    currEnemy.currentEnemyLeft = currentEnemyLeft;
   })
 }
 
 function updateEnemies() {
-  // moveEnemies();
+  moveEnemies();
 
-  if(enemies.length < 1) {
+  const enemies = document.querySelectorAll('.enemy');
+  if(enemies.length < enemyLimit) {
     addEnemy();
   }
   // console.log(enemies);
 }
 
+function evaluateEnemyVsEnemyCollision() {
+  const enemies = document.querySelectorAll('.enemy');
+
+  enemies.forEach((enemyOne, enemyOneIndex) => {
+    const enemyOneId = enemyOne.id;
+
+    enemies.forEach((enemyTwo, enemyTwoIndex) => {
+      const enemyTwoId = enemyTwo.id;
+
+      if(enemyOneId !== enemyTwoId) {
+        let currentEnemyTopOne = parseFloat(enemyOne.style.top);
+        let currentEnemyLeftOne = parseFloat(enemyOne.style.left);
+        let currentEnemyBottomOne = currentEnemyTopOne + 2;
+        let currentEnemyRightOne = currentEnemyLeftOne + 2;
+
+        let currentEnemyTopTwo = parseFloat(enemyTwo.style.top);
+        let currentEnemyLeftTwo = parseFloat(enemyTwo.style.left);
+        let currentEnemyBottomTwo = currentEnemyTopTwo + 2;
+        let currentEnemyRightTwo = currentEnemyLeftTwo + 2;
+
+        if(
+          currentEnemyLeftOne < currentEnemyRightTwo &&
+          currentEnemyRightOne > currentEnemyLeftTwo &&
+          currentEnemyTopOne < currentEnemyBottomTwo &&
+          currentEnemyBottomOne > currentEnemyTopTwo
+        ) {
+          const leftOverlap = Math.abs(currentEnemyLeftOne - currentEnemyRightTwo);
+          const topOverlap = Math.abs(currentEnemyTopOne - currentEnemyBottomTwo);
+          const rightOverlap = Math.abs(currentEnemyRightOne - currentEnemyLeftTwo);
+          const bottomOverlap = Math.abs(currentEnemyBottomOne - currentEnemyTopTwo);
+
+          if(currentEnemyLeftOne < currentEnemyRightTwo && currentEnemyRightOne > currentEnemyRightTwo) {
+            if(currentEnemyTopOne < currentEnemyBottomTwo && currentEnemyBottomOne > currentEnemyBottomTwo) {
+              if(leftOverlap <= topOverlap) {
+                currentEnemyLeftOne = currentEnemyRightTwo - (leftOverlap/2);
+                currentEnemyLeftTwo = currentEnemyLeftOne - 2;
+              } else {
+                currentEnemyTopOne = currentEnemyBottomTwo - (topOverlap/2);
+                currentEnemyTopTwo = currentEnemyTopOne - 2;
+              }
+            } else if(currentEnemyBottomOne > currentEnemyTopTwo && currentEnemyTopOne < currentEnemyTopTwo) {
+              if(leftOverlap <= bottomOverlap) {
+                currentEnemyLeftOne = currentEnemyRightTwo - (leftOverlap/2);
+                currentEnemyLeftTwo = currentEnemyLeftOne - 2;
+              } else {
+                currentEnemyTopOne = currentEnemyTopTwo - 2 + (bottomOverlap/2);
+                currentEnemyTopTwo = currentEnemyTopOne + 2;
+              }
+            }
+          } else if(currentEnemyRightOne > currentEnemyLeftTwo && currentEnemyLeftOne < currentEnemyLeftTwo) {
+            if(currentEnemyTopOne < currentEnemyBottomTwo && currentEnemyBottomOne > currentEnemyBottomTwo) {
+              if(rightOverlap <= topOverlap) {
+                currentEnemyLeftOne = currentEnemyLeftTwo - 2 + (rightOverlap/2);
+                currentEnemyLeftTwo = currentEnemyLeftOne + 2;
+              } else {
+                currentEnemyTopOne = currentEnemyBottomTwo - (topOverlap/2);
+                currentEnemyTopTwo = currentEnemyTopOne - 2;
+              }
+            } else if(currentEnemyBottomOne > currentEnemyTopTwo && currentEnemyTopOne < currentEnemyTopTwo) {
+              if(rightOverlap <= bottomOverlap) {
+                currentEnemyLeftOne = currentEnemyLeftTwo - 2 + (rightOverlap/2);
+                currentEnemyLeftTwo = currentEnemyLeftOne + 2;
+              } else {
+                currentEnemyTopOne = currentEnemyTopTwo - 2 + (bottomOverlap/2);
+                currentEnemyTopTwo = currentEnemyTopOne + 2;
+              }
+            }
+          }
+        }
+
+        enemyOne.style.top = currentEnemyTopOne + 'vw';
+        enemyOne.style.left = currentEnemyLeftOne + 'vw';
+        enemyTwo.style.top = currentEnemyTopTwo + 'vw';
+        enemyTwo.style.left = currentEnemyLeftTwo + 'vw';
+      }
+    })
+  })
+}
+
+function evaluateEnemyVsBulletCollision() {
+  const enemies = document.querySelectorAll('.enemy');
+  const bullets = document.querySelectorAll('.bullet');
+
+  let deadEnemies = [];
+  let deadBullets = [];
+  enemies.forEach((enemy) => {
+    let currentEnemyTop = parseFloat(enemy.style.top);
+    let currentEnemyLeft = parseFloat(enemy.style.left);
+    let currentEnemyBottom = currentEnemyTop + 2;
+    let currentEnemyRight = currentEnemyLeft + 2;
+
+    bullets.forEach((bullet) => {
+      let currentBulletTop = parseFloat(bullet.style.top);
+      let currentBulletLeft = parseFloat(bullet.style.left);
+      let currentBulletBottom = currentBulletTop + 2;
+      let currentBulletRight = currentBulletLeft + 2;
+      // console.log(currentBulletTop + " " + currentBulletLeft + " " + currentBulletBottom + " " + currentBulletRight);
+
+      if(
+        currentBulletLeft < currentEnemyRight &&
+        currentBulletRight > currentEnemyLeft &&
+        currentBulletTop < currentEnemyBottom &&
+        currentBulletBottom > currentEnemyTop
+      ) {
+        deadEnemies.push(enemy);
+        deadBullets.push(bullet);
+        // bullet.remove();
+      }
+    })
+  })
+  // if(deadEnemies.length > 0) {
+  //   console.log(deadEnemies);
+  // }
+  deadEnemies.forEach((enemy) => {
+    enemy.remove();
+  })
+  deadBullets.forEach((bullet) => {
+    bullet.remove();
+  })
+}
+
 function evaluateCollision() {
   evaluatePlayerCharacterCollision();
+  evaluateEnemyVsBulletCollision();
+  evaluateEnemyVsEnemyCollision();
+}
+
+function displayGameOver() {
+  const gameOver = document.createElement('div');
+  gameOver.classList.add('gameover');
+  gameOver.style.width = '70vw';
+  gameOver.style.height = '35vw';
+  gameOver.style.backgroundColor = 'white';
+  gameOver.style.opacity = '0.8';
+  gameOver.style.fontSize = '15vw';
+  gameOver.style.textAlign = 'center';
+  gameOver.innerHTML = 'GAME OVER';
+  content.appendChild(gameOver);
 }
 
 function createNewGameCycle() {
   movePlayerCharacter();
-  // updateBullets();
+  updateBullets();
   updateEnemies();
   evaluateCollision();
+
+  console.log(playerHealth);
+  if(playerHealth <= 0) {
+    clearInterval(interval);
+    displayGameOver();
+  }
 }
 
 addPlayerCharacter();
 
-let interval = setInterval(createNewGameCycle, 16.667);
+let interval = setInterval(createNewGameCycle, 16.666);
 
 document.addEventListener('keydown', keydown)
 document.addEventListener('keyup', keyup)
